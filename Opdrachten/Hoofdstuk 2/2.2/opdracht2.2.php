d1f2b0a91e2c7ae4c58ed4d7da7af46cac693538
Miquel <miquel.dewit@edu-kw1c.nl>
2020-02-11 17:27:32 +0100

    Opdracht 2.2 PHP

---
+++ Opdrachten/Hoofdstuk 2/2.2/opdracht2.2.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * User: Miquel de Wit
+ * Date: 11-2-2020
+ * Time: 17:04
+ * File: opdracht2.2.php
+ */
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="description" content=" ">
+        <meta name="keywords" content=" ">
+        <meta charset="utf-8">
+        <title>
+            Opdracht 2.2
+        </title>
+    </head>
+    <body>
+        <?php
+            $text1 = "Hallo";
+            $text2 = "een makkelijke taal";
+            $text3 = "toch zo'n makkelijke taal";
+            $text4 = "wat is";
+            $text5 = "PHP";
+            $text6 = "Nooit gedacht dat";
+            $text7 = "De installatie is best ingewikkeld";
+            $text8 = "Fijn";
+            $text9 = "?";
+            $text10 = ".";
+            $text11 = ",";
+            $text12 = "<br>";
+            $text13 = "is";
+            $text14 = "Vind je niet";
+            $text15 = "toch";
+            $text16 = "dat";
+
+            echo("<p>" . $text1 . $text11 . " " . $text4 . " " . $text5 . " " . $text3 . $text10 . $text12 . $text7 . $text10 . " " . $text8 . " " . $text15 . $text9 . $text12 . $text6 . " " . $text5 . " " . $text3 . " " . $text13 . $text10 . "</p>");
+
+            echo("<p>". $text1 . $text11 . $text12 . $text8 . " " . $text15 . " " . $text16 . " " . $text5 . " " . $text2 . " " . $text13 . $text10 . $text12 . $text7 . $text10 . " " . $text14 . $text9 . "</p>");
+            ?>
+    </body>
+</html>
