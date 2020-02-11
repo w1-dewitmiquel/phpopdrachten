65a56052e31e2b259fd6208b0db9a2fbb299c731
Miquel <miquel.dewit@edu-kw1c.nl>
2020-02-11 14:11:59 +0100

    t

---
+++ Opdrachten/Hoofdstuk 2/2.1/Opdracht2.1.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * User: Miquel de Wit
+ * Date: 11-2-2020
+ * Time: 12:31
+ * File: opdracht2.1.php
+ */
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="description" content=" ">
+        <meta name="keywords" content=" ">
+        <meta charset="utf-8">
+        <title>
+            Opdracht 2.1
+        </title>
+    </head>
+    <body>
+        <?php
+            echo("<p>Hello World</p>");
+        ?>
+    </body>
+</html>
